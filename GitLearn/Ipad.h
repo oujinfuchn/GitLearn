6447f54527d5aa9d2801b0991b51f2e4042cdd1f
Edison <Edison@edison-oudeMacBook-Pro-2.local>
2019-02-22 20:13:55 +0800

    ipad

---
+++ GitLearn/Ipad.h
@@ -0,0 +1,17 @@
+//
+//  Ipad.h
+//  GitLearn
+//
+//  Created by Edison on 2019/2/22.
+//  Copyright © 2019年 Edison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Ipad : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitLearn/Ipad.m
@@ -0,0 +1,13 @@
+//
+//  Ipad.m
+//  GitLearn
+//
+//  Created by Edison on 2019/2/22.
+//  Copyright © 2019年 Edison. All rights reserved.
+//
+
+#import "Ipad.h"
+
+@implementation Ipad
+
+@end
