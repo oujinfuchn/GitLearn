414ef9d08c818b0486c79a4747053437f4c96e94
Edison <Edison@edison-oudeMacBook-Pro-2.local>
2019-02-22 20:07:44 +0800

    dog

---
+++ GitLearn/Dog.m
@@ -0,0 +1,28 @@
+//
+//  Dog.m
+//  GitLearn
+//
+//  Created by Edison on 2019/2/22.
+//  Copyright © 2019年 Edison. All rights reserved.
+//
+
+#import "Dog.h"
+
+
+@interface Dog ()
+
+@end
+
+@implementation Dog
+
+- (void)setName:(NSString *)name {
+    
+
+}
+
+- (NSString *)name {
+    
+    return @"lisi";
+}
+
+@end
